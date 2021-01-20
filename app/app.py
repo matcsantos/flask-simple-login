615722a480f13b4c658a86116c95ab0d00643072
Matheus Carvalho <matcarvalhosantos@outlook.com>
2021-01-20 11:49:15 -0300

    feat: add router

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
 
 from flask import Flask
+from routes import router
 
 def create_app():
+    
     app = Flask(__name__)
     
+    for blueprint in router.blueprints:
+        app.register_blueprint(blueprint)
+
     return app
