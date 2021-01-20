2d0e839c1b4330443a7231a146e4638e01b22480
Matheus Carvalho <matcarvalhosantos@outlook.com>
2021-01-20 13:44:27 -0300

    feat: add user page

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,7 +1,16 @@
 
-from flask import render_template
+from flask import render_template, session
 
 class HomeController:
 
     def index():
+        user = session.get('user')
+        
+        if user:
+            context = dict(
+                user = user,
+                permanent = session.permanent
+            )
+            return render_template('user.html', **context)
+
         return render_template('index.html')
